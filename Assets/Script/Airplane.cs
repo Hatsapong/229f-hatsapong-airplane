a173640964cfd209aae3cfc89858e80a07e7f1ce
hatsapong <hatsapong.api@gmail.com>
2025-03-06 18:15:51 +0700

    Add Airplane

---
+++ Assets/Script/Airplane.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Airplane : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float enginePower = 20f;
+    [SerializeField] float liftBooster = 0.5f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
+    [SerializeField] float yawPower = 50f;
+    [SerializeField] float pitchPower = 50f;
+    [SerializeField] float rollPower = 30f;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space)) ;
+        {
+            rb.AddForce(transform.forward * enginePower);
+        }
+    }
+}
